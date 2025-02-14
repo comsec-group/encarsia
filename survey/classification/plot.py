ade160a9a3ced634758fd3f15f23b40406874449
mboelcskei <mboelcskei@ethz.ch>
2025-02-14 01:27:41 +0100

    Includes classification plots

--- survey/classification/plot.py
+++ survey/classification/plot.py
@@ -4,7 +4,7 @@ import numpy as np
 from collections import defaultdict
 
 
-def plot_stacked_column(datasets, key="category"):
+def plot_stacked_column(filename, datasets, key="category"):
     category_counts = defaultdict(lambda: {name: 0 for name in datasets.keys()})
 
     for dataset_name, dataset_points in datasets.items():
@@ -32,12 +32,17 @@ def plot_stacked_column(datasets, key="category"):
 
     plt.xlabel(key.capitalize())
     plt.ylabel("No. Bugs")
-    plt.legend()
-    plt.title(f"Bugs by {key.capitalize()}")
-    plt.savefig(f"{key}.png")
+    plt.legend(loc='upper left')
+    plt.title(f"{filename.capitalize()} bugs by {key.capitalize()}")
+    plt.savefig(f"{filename}.png")
 
 
 if __name__ == "__main__":
     with open("natural.json", "r") as file:
         data = json.load(file)
-    plot_stacked_column(data)
+    plot_stacked_column("natural", data)
+    
+    plt.clf()
+    with open("synthetic.json", "r") as file:
+        data = json.load(file)
+    plot_stacked_column("synthetic", data)
